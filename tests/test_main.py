b9411a3aed9dfab6a0b02c02ca6a4a89444946a0
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-05-05 10:18:52 +0000

    tests - add sanity fixtures

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,30 @@
 from gameoflife import __version__
-
+import pytest
 import gameoflife.main  #explicitly maintain namespace information for debugging
 
-#general testing
+#Sanity checks
 def test_main_module_find():
     assert gameoflife.main.module_sanity_check() == 1
 
 def test_version():
     assert __version__ == '0.1.0'
 
+@pytest.fixture #fixture supplies test input data
+def test_fixture_input():
+    """
+    Does the pytest fixture work?
+    """
+    return "hello"
+
+def test_fixture_input_working(test_fixture_input):
+    assert test_fixture_input == "hello"
 
 # Test board states
 
-## Board initialized with random values?
 
+
+# Board initialized with random values?
+
+
+
+# count live neighbors
