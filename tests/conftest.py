43658e55f5870e83cc12aaccd0626cb6aeea7f43
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-05-07 10:12:39 +0000

    tests - add array sanity checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,39 @@
+import gameoflife.main  # explicitly maintain namespace information for debug
 import pytest
 
 """
 Single source of truth for fixtures across tests
 """
 
+# Scopes of pytest fixtures decide how often they are run.
+# Syntax: @pytest.fixture(scope="function")
+# The default scope of a fixture is function; this means:
+# the fixture is setup & tore down after every function call.
+# Other possible fixtures are: session, class, module
+# You can also make a fixture apply to every test with autouse
+# Syntax: @pytest.fixture(autouse=True)
+
 
 @pytest.fixture
 def sanity_check_fixture():
     return "testinput"
+
+
+@pytest.fixture
+def sanity_check_array():
+    return [1, 1, 1, 1, 1]
+
+
+@pytest.fixture
+def sanity_check_nested_array():
+    return [[1, 1], [1], [1, 1, 1]]
+
+
+@pytest.fixture
+def random_board_fixture():
+    return gameoflife.main.Board(randomize_seed=1)
+
+
+@pytest.fixture
+def dead_board_fixture():
+    return gameoflife.main.Board()
--- tests/test_main.py
+++ tests/test_main.py
@@ -19,7 +19,37 @@ def test_fixture_input_via_conftest(sanity_check_fixture):
     assert sanity_check_fixture == "testinput"
 
 
+def test_multi_assert_single_array(sanity_check_array):
+    """Can you do multiple asserts on an array?"""
+    for el in sanity_check_array:
+        assert el == 1
+
+
+def test_multi_assert_nested_array(sanity_check_nested_array):
+    """Can you do multiple asserts on a nested array"""
+    for row in sanity_check_nested_array:
+        for el in row:
+            assert el == 1
+
+
 # Test board states
+
+
+def test_init_board(dead_board_fixture):
+    """
+    Does the board get initialized?
+    """
+    assert dead_board_fixture  # returns true if object exists
+
+
+def test_dead_board_next_state(dead_board_fixture):
+    """
+    Does the dead board stay dead?
+    Or have we created life out of nothing all of the sudden?
+    """
+    for cell in dead_board_fixture.next_state():
+        assert cell == 0
+
 
 # Board initialized with random values?
 
