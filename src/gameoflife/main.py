f082476753ae8526bd5d4bec5ffcb48187a46d3a
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-05-08 21:26:06 +0000

    ci - ensure consistent os versions

--- src/gameoflife/main.py
+++ src/gameoflife/main.py
@@ -21,6 +21,7 @@ class Board:
     ):
         # size 2-dimensional?
         self.init_args = locals()  # store called arguments for debugging
+        print(self.init_args)
         if self.check_size(*size) == 1:
             sys.exit("error in size argument")
         x, y = size
